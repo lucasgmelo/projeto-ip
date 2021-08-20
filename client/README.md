64113d46e51aeb55084a16d15925798a075002a3
lucasgmelo <contatolucasgmelo@gmail.com>
2021-08-19 21:32:54 -0300

    Criar Boilerplate da Landing Page

---
+++ client/README.md
@@ -0,0 +1,11 @@
+ <h1>:sparkles: hi!</h1>
+
+<p>this is my fav boilerplate. honestly, i created it because i was tired of doing the same process as always!!!<p>
+  
+<h2>:giraffe: techs</h2>
+<h3>:sauropod: eslint</h3>
+<p>easy code formatter to make everything prettier</p>
+<h3>:sauropod: typescript</h3>
+<p>easy configured</p>
+<h3>:sauropod: styled components</h3>
+<p>added and a global style created. edit it as you want.</p>
---
+++ client/src/styles/global.ts
@@ -0,0 +1,58 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+:root {
+  --bg: #1e1e26;
+  --bg-light: #1a1a21;
+  --bg-dark: #16161c;
+  --gradient: linear-gradient(249.73deg, #f29682 0%, #ee69ac 50%, #cb4bcf 100%);
+  --gray: #D7D7D7;
+  --info: #BBB;
+}
+
+  * {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  color: #fff;
+}
+
+@media (max-width: 1080px) {
+  html {
+      font-size: 93.75%;
+  }
+}
+
+@media (max-width: 720px) {
+  html {
+      font-size: 87.5%;
+  }
+}
+
+
+body {
+  font: 16px "Inter", sans-serif;
+  color: #f9f9f9;
+  background: var(--bg);
+
+  max-height: 100vh;
+}
+
+body, input, textarea, select, button {
+  font: 400 1rem "Inter", sans-serif;
+}
+
+button {
+  cursor: pointer;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+h1, label {
+    font-size: 1.5rem;
+    font-weight: 600;
+}
+`;
